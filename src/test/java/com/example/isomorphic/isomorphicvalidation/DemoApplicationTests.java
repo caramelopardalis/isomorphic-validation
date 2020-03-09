1ecda938ea12f94bbd4487dcd4829a27ab4003bc
caramelopardalis <ltsu.koiwai@gmail.com>
2020-03-09 21:58:06 +0900

    Initial commit

---
+++ src/test/java/com/example/isomorphic/isomorphicvalidation/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.isomorphic.isomorphicvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
